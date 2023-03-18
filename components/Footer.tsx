ace31bc202afe02fd165724992142e1b7e21bfff
J.L.C.A <jorgelca1205@gmail.com>
2023-03-18 11:57:59 -0600

    fix(Footer): Links must have discernible text

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,34 +7,44 @@ import Wootsbot from '@/design-system/icons/Wootsbot';
 function Footer() {
   return (
     <footer className="antialiased px-8 max-w-[65ch] mx-auto text-center mb-24">
-      <Link href="/" className="flex flex-col items-center justify-center mb-10 mt-24">
+      <Link
+        aria-label="Logotipo de Wootsbot que redirige a la página de inicio"
+        href="/"
+        className="flex flex-col items-center justify-center mb-10 mt-24"
+      >
         <Wootsbot size={45} />
       </Link>
 
       <ul className="list-unstyled flex justify-center flex-wrap gap-4 text-sm">
         <li>
-          <Link className="underline" href="/transparency">
+          <Link aria-label="Link to Mi misión" className="underline" href="/transparency">
             Mi misión
           </Link>
         </li>
         <li>
-          <Link className="underline font-normal" href="/">
+          <Link aria-label="Link to Sobre mi" className="underline font-normal" href="/">
             Sobre mi
           </Link>
         </li>
         <li>
-          <Link className="underline" rel="noopener noreferrer" href="/uses">
+          <Link aria-label="Link to Uses" className="underline" rel="noopener noreferrer" href="/uses">
             Uses
           </Link>
         </li>
         <li>
-          <Link className="underline" rel="noopener noreferrer" href="/blog">
+          <Link aria-label="Link to Articulos" className="underline" rel="noopener noreferrer" href="/blog">
             Articulos
           </Link>
         </li>
 
         <li>
-          <Link className="underline" target="_blank" rel="noopener noreferrer" href="/feed.xml">
+          <Link
+            aria-label="Link to Rss"
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="/feed.xml"
+          >
             Rss
           </Link>
         </li>
@@ -42,17 +52,30 @@ function Footer() {
 
       <ul className="list-unstyled flex justify-center flex-wrap gap-4 text-sm mt-5">
         <li>
-          <a className="underline" target="_blank" rel="noopener noreferrer" href="https://github.com/wootsbot">
+          <Link
+            aria-label="Link to Github"
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://github.com/wootsbot"
+          >
             <SiGithub size={20} />
-          </a>
+          </Link>
         </li>
         <li>
-          <Link className="underline" target="_blank" rel="noopener noreferrer" href="https://twitter.com/wootsbot">
+          <Link
+            aria-label="Link to Twitter"
+            className="underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://twitter.com/wootsbot"
+          >
             <SiTwitter size={20} />
           </Link>
         </li>
         <li>
           <Link
+            aria-label="Link to Stackoverflow"
             className="underline"
             target="_blank"
             rel="noopener noreferrer"
@@ -63,6 +86,7 @@ function Footer() {
         </li>
         <li>
           <Link
+            aria-label="Link to Instagram"
             className="underline"
             target="_blank"
             rel="noopener noreferrer"
@@ -73,6 +97,7 @@ function Footer() {
         </li>
         <li>
           <Link
+            aria-label="Link to Polywork "
             className="underline"
             target="_blank"
             rel="noopener noreferrer"

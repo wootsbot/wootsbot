3931145e3ee1949ac5fc573f3c841c3a3d7ecce2
J.L.C.A <jorgelca1205@gmail.com>
2023-04-16 15:46:35 -0600

    chore: use cicada

---
+++ .cicada/run.ts
@@ -0,0 +1,31 @@
+import { Job, Pipeline } from 'https://deno.land/x/cicada@v0.1.34/lib.ts';
+
+const job = new Job({
+  name: 'Build app',
+  image: 'ubuntu:22.04',
+  steps: [
+    {
+      name: 'Print a message',
+      run: 'echo Hello, world!',
+    },
+    {
+      name: 'Run a js function',
+      run: () => {
+        console.log('Hello from js');
+      },
+    },
+  ],
+});
+
+const muslJob = new Job({
+  name: 'musl_job',
+  image: 'ubuntu-latest',
+  steps: [
+    'corepack enable',
+    'corepack prepare pnpm@7.30.5 --activate',
+    'pnpm install --no-frozen-lockfile',
+    'pnpm build',
+  ],
+});
+
+export default new Pipeline([job, muslJob]);

59b9f71f09956456a135b46b67ab030938053862
J.L.C.A <jorgelca1205@gmail.com>
2023-05-21 23:01:18 -0600

    refactor(UI): Use new UI

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -151,8 +151,8 @@ function BlogPage({ params }) {
             />
           )}
           <figcaption className="flex flex-row items-center space-x-2 mt-2 justify-end">
-            <PhotoIcon className="w-4 text-gray-600" />
-            <p className="text-gray-600 text-xs">{post?.creditImage ?? 'Crédito de la imagen por Jorge L. Calleja'}</p>
+            <PhotoIcon className="w-4 text-gray-400" />
+            <p className="text-gray-400 text-xs">{post?.creditImage ?? 'Crédito de la imagen por Jorge L. Calleja'}</p>
           </figcaption>
         </div>
       </div>

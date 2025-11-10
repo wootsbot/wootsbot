c99b4e96a2998712505316dcfcbf094122777712
Jorge Luis Calleja Alvarado <jorgelca1205@gmail.com>
2025-11-10 00:15:19 -0600

    fix(core) Fixed metadata in posts

--- src/app/(galery)/photos/page.tsx
+++ src/app/(galery)/photos/page.tsx
@@ -1,3 +1,5 @@
+import type { Metadata } from "next";
+
 import { PhotoGalleryAll } from "#/components/photos/photo-gallery-all";
 
 import raw from "#/lib/photos";
@@ -5,3 +7,22 @@ import raw from "#/lib/photos";
 export default async function PhotosPage() {
   return <PhotoGalleryAll photos={raw} />;
 }
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  return {
+    title: "Photos",
+    description: "Photos by wootsbot",
+    creator: "Jorge Luis Calleja Alvarado",
+    openGraph: {
+      title: "Photos by wootsbot",
+      description: "Photos by wootsbot",
+      type: "article",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "Photos by wootsbot",
+      description: "Photos by wootsbot",
+      creator: "Jorge Luis Calleja Alvarado",
+    },
+  };
+};
